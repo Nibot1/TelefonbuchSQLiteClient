7345a9f3b503c0983cddf14651d07153f4cbe874
Tobin Rosenau <tobin.rosenau@mail.de>
2017-12-12 22:01:25 +0100

    fixed the communication with the Server

--- src/Client.java
+++ src/Client.java
@@ -19,33 +19,33 @@ public class Client {
 	      InputStreamReader PortLeser = new InputStreamReader(sock.getInputStream());
 	      BufferedReader Eingabe= new BufferedReader(PortLeser);
 		//Closes the Server when the Program is closed;
-		  		  
 		  Scanner scanner = new Scanner(System.in);
-		  System.out.println("Wähle eine Option aus 1:Erstellen 2:Anzeigen 3:Löschen 4:Suchen : ");
-		  String option = scanner.nextLine();
-		  if(option.toLowerCase() == "erstellen" || option == "1") {
+		  System.out.print("Wähle eine Option aus 1:Erstellen 2:Anzeigen 3:Löschen 4:Suchen : ");
+		  int option = scanner.nextInt();
+		  if(option == 1) {
+			  //Create a StringBuilder and fill it in Json Format with the User input
 			  StringBuilder sb = new StringBuilder();
-			  System.out.println("Gib die Daten von dem neuen Kontakt ein.");
+			  System.out.print("Gib die Daten von dem neuen Kontakt ein.");
 			  sb.append("{");
-			  System.out.println("Vorname: ");
+			  System.out.print("Vorname: ");
 			  sb.append("\"Vorname\":\""+scanner.next()+"\",");
-			  System.out.println("Nachname: ");
+			  System.out.print("Nachname: ");
 			  sb.append("\"Nachname\":\""+scanner.next()+"\",");
-			  System.out.println("Straße: ");
+			  System.out.print("Straße: ");
 			  sb.append("\"Straße\":\""+scanner.next()+"\",");
-			  System.out.println("Hausnummer: ");
+			  System.out.print("Hausnummer: ");
 			  sb.append("\"Hausnummer\":\""+scanner.next()+"\",");
-			  System.out.println("Postleitzahl: ");
+			  System.out.print("Postleitzahl: ");
 			  sb.append("\"Postleitzahl\":\""+scanner.next()+"\",");
-			  System.out.println("Ort: ");
+			  System.out.print("Ort: ");
 			  sb.append("\"Ort\":\""+scanner.next()+"\",");
-			  System.out.println("Telefonnummer: ");
+			  System.out.print("Telefonnummer: ");
 			  sb.append("\"Telefonnummer\":\""+scanner.next()+"\",");
-			  System.out.println("Faxnummer: ");
+			  System.out.print("Faxnummer: ");
 			  sb.append("\"Faxnummer\":\""+scanner.next()+"\",");
-			  System.out.println("Handynummer: ");
+			  System.out.print("Handynummer: ");
 			  sb.append("\"Handynummer\":\""+scanner.next()+"\",");
-			  System.out.println("Email: ");
+			  System.out.print("Email: ");
 			  sb.append("\"Email\":\""+scanner.next()+"\"");
 			  sb.append("}");
 			  //Send a String to the Server
@@ -54,31 +54,30 @@ public class Client {
 		      String S=Eingabe.readLine();
 		      //Prints the answer from the Server
 		      System.out.println(S);
-		      //scanner.close();
 			  
-		  }if(option.toLowerCase() == "anzeigen" || option == "2") {
-			  
+		  }if(option ==2) {
 			//Send a String to the Server
-		      
 		      Ausgabe.println("anzeigen");
 		      //Reads the answer from  the Server
 		      String S=Eingabe.readLine();
 		      //Prints the answer from the Server
 		      System.out.println(S);
+		      scanner.close();
+
 			  
-		  }if(option.toLowerCase() == "löschen" || option == "3") {
-			  
+		  }if(option == 3) {
 			//Send a String to the Server
 		      Ausgabe.println("löschen");
 		      //Reads the answer from  the Server
 		      String S=Eingabe.readLine();
 		      //Prints the answer from the Server
 		      System.out.println(S);
+		      scanner.close();
+
 			  
-		  }if(option.toLowerCase() == "suchen" || option == "4") {
+		  }if(option == 4) {
 			//Send command String to the Server
 		      Ausgabe.println("suchen");
-		      
 		      //Send search String to the Server
 		      System.out.print("Geben sie das Suchwort ein: ");
 		      Ausgabe.println(scanner.next());
@@ -86,11 +85,12 @@ public class Client {
 		      String S=Eingabe.readLine();
 		      //Prints the answer from the Server
 		      System.out.println(S);
+		      scanner.close();
 			  
 		  }else {
-			  //scanner.close();
+			  scanner.close();
 		  }
-		  //scanner.close();
+		  scanner.close();
 	}
 	}
 
