44836740949e6eb379633a9ac76ef55ccef656e2
Tobin Rosenau <tobin.rosenau@mail.de>
2017-12-12 21:44:28 +0100

    added comandline interface

--- src/Client.java
+++ src/Client.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 /**
  * @author Tobin Rosenau
@@ -12,32 +13,85 @@ import java.net.UnknownHostException;
 public class Client {
 	static Socket sock; 
 	public static void main(String[] args) throws IOException, UnknownHostException {
-		// Start a Socket on localhost port 6001
+		// Start a Socket on localhost port 6002
 		sock= new Socket("localhost",6002);
-		//Closes the Server when the Program is closed;
-		  Runtime.getRuntime().addShutdownHook(new Thread()
-			{
-			    @Override
-			    public void run()
-			    {
-			        try {
-						sock.close();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-			    }
-			});
-		  //Send a String to the Server
-	      PrintWriter Ausgabe= new PrintWriter(sock.getOutputStream(),true);
-	      Ausgabe.println("Guten Tag und viel Spass!");
-	      //Reads the answer from  the Server
+		 PrintWriter Ausgabe= new PrintWriter(sock.getOutputStream(),true);
 	      InputStreamReader PortLeser = new InputStreamReader(sock.getInputStream());
 	      BufferedReader Eingabe= new BufferedReader(PortLeser);
-	      String S=Eingabe.readLine();
-	      //Prints the answer from the Server
-	      System.out.println("Antwort vom Server: "+S);
-
+		//Closes the Server when the Program is closed;
+		  		  
+		  Scanner scanner = new Scanner(System.in);
+		  System.out.println("Wähle eine Option aus 1:Erstellen 2:Anzeigen 3:Löschen 4:Suchen : ");
+		  String option = scanner.nextLine();
+		  if(option.toLowerCase() == "erstellen" || option == "1") {
+			  StringBuilder sb = new StringBuilder();
+			  System.out.println("Gib die Daten von dem neuen Kontakt ein.");
+			  sb.append("{");
+			  System.out.println("Vorname: ");
+			  sb.append("\"Vorname\":\""+scanner.next()+"\",");
+			  System.out.println("Nachname: ");
+			  sb.append("\"Nachname\":\""+scanner.next()+"\",");
+			  System.out.println("Straße: ");
+			  sb.append("\"Straße\":\""+scanner.next()+"\",");
+			  System.out.println("Hausnummer: ");
+			  sb.append("\"Hausnummer\":\""+scanner.next()+"\",");
+			  System.out.println("Postleitzahl: ");
+			  sb.append("\"Postleitzahl\":\""+scanner.next()+"\",");
+			  System.out.println("Ort: ");
+			  sb.append("\"Ort\":\""+scanner.next()+"\",");
+			  System.out.println("Telefonnummer: ");
+			  sb.append("\"Telefonnummer\":\""+scanner.next()+"\",");
+			  System.out.println("Faxnummer: ");
+			  sb.append("\"Faxnummer\":\""+scanner.next()+"\",");
+			  System.out.println("Handynummer: ");
+			  sb.append("\"Handynummer\":\""+scanner.next()+"\",");
+			  System.out.println("Email: ");
+			  sb.append("\"Email\":\""+scanner.next()+"\"");
+			  sb.append("}");
+			  //Send a String to the Server
+		      Ausgabe.println(sb.toString());
+		      //Reads the answer from  the Server
+		      String S=Eingabe.readLine();
+		      //Prints the answer from the Server
+		      System.out.println(S);
+		      //scanner.close();
+			  
+		  }if(option.toLowerCase() == "anzeigen" || option == "2") {
+			  
+			//Send a String to the Server
+		      
+		      Ausgabe.println("anzeigen");
+		      //Reads the answer from  the Server
+		      String S=Eingabe.readLine();
+		      //Prints the answer from the Server
+		      System.out.println(S);
+			  
+		  }if(option.toLowerCase() == "löschen" || option == "3") {
+			  
+			//Send a String to the Server
+		      Ausgabe.println("löschen");
+		      //Reads the answer from  the Server
+		      String S=Eingabe.readLine();
+		      //Prints the answer from the Server
+		      System.out.println(S);
+			  
+		  }if(option.toLowerCase() == "suchen" || option == "4") {
+			//Send command String to the Server
+		      Ausgabe.println("suchen");
+		      
+		      //Send search String to the Server
+		      System.out.print("Geben sie das Suchwort ein: ");
+		      Ausgabe.println(scanner.next());
+		      //Reads the answer from  the Server
+		      String S=Eingabe.readLine();
+		      //Prints the answer from the Server
+		      System.out.println(S);
+			  
+		  }else {
+			  //scanner.close();
+		  }
+		  //scanner.close();
+	}
 	}
 
-	}
+	
